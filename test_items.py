17898589568a7d2c1588ccac311e7af729f3bbde
Olga_Eremkina <olga_eremkina@rntgroup.com>
2023-07-31 14:24:53 +0300

    update test

--- test_items.py
+++ test_items.py
@@ -3,10 +3,9 @@ import time
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-class TestPage:
-    def test_button_is_visible(self, browser):
-        browser.get(link)
-        time.sleep(5)
-        assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
 
 
+def test_button_is_visible(browser):
+    browser.get(link)
+    time.sleep(5)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
