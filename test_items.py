8e66e1179a64f870418668265716fd74650abe8a
Olga_Eremkina <olga_eremkina@rntgroup.com>
2023-07-31 15:42:24 +0300

    update

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_is_visible(browser):
+def test_basket_button_is_visible(browser):
     browser.get(link)
     time.sleep(5)
     assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
